dd716580378123b2ccb3a1a778a892b5a3cf5dc1
aydinuslu <uslu.aydin@gmail.com>
2024-03-30 08:53:13 +0300

    2024-03-27

---
+++ MyDesign1/app/src/main/java/com/example/mydesign1/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.mydesign1
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //R = res klasörü (Kotlin classı içinde
+        //@ = res klasörü (XML dosyası içinde)
+
+    }
+}
