982caed0f57b3111291f5ef8cb2697466da4c0ac
aydinuslu <uslu.aydin@gmail.com>
2024-04-03 21:54:25 +0300

    2024-04-03

--- WidgetsKullanimi/app/src/main/java/com/example/widgetskullanimi/MainActivity.kt
+++ WidgetsKullanimi/app/src/main/java/com/example/widgetskullanimi/MainActivity.kt
@@ -5,16 +5,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.widgetskullanimi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonResim1.setOnClickListener {
+            binding.imageView.setImageResource(R.drawable.resim1)
         }
+
+        binding.buttonResim2.setOnClickListener {
+            binding.imageView.setImageResource(resources.getIdentifier("resim2", "drawable", packageName))
+        }
+
+
     }
 }
